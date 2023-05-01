92eaa054c078e2c48d861a344eaca0de6c197761
NAJJUU <122864238+NAJJUU@users.noreply.github.com>
2023-05-01 09:53:15 +0900

    Controller 경로 설정

--- OTTT/src/main/java/com/ottt/ottt/controller/workDetailPage/reviewController.java
+++ OTTT/src/main/java/com/ottt/ottt/controller/workDetailPage/reviewController.java
@@ -1,4 +1,4 @@
-package com.ottt.ottt;
+package com.ottt.ottt.controller.workDetailPage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
